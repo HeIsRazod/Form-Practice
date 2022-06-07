799d69cfacc4195dd922f8b835d639684c6d5167
HeIsRazod <100656213+HeIsRazod@users.noreply.github.com>
2022-06-07 19:46:17 -0300

    formulario de parcial web 7/6

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,68 +1,65 @@
 
 function enviar() {
 
-    if (validar()) {
+    if (validar()){
 
+        let nombre = document.querySelector("#nombre");
+        let age = document.querySelector("#age");
+        let email = document.querySelector("#email");
 
+        let p = document.createElement("p");
+        p.innerHTML = nombre.value +" de " + age.value +" años de edad se suscribio con el email " + email.value;
 
-
+        document.getElementById("msj").appendChild(p);
     }
-
-
 
     return false;
 }
 
 function validar() {
 
-    var nick = document.querySelector("#nick");
-    var age = document.querySelector("#age");
-    var razas = document.querySelector("#razas");
-    var genero = document.querySelector("#genero");
-    var arma = document.querySelector("#opc1");
-    var habilidad = document.querySelector("#opc2");
-    var texto = document.querySelector("#texto");
-    var weapon = document.querySelector("#tipoarma");
-
+    let nombre = document.querySelector("#nombre");
+    let age = document.querySelector("#age");
+    let email = document.querySelector("#email");
     let errores = [];
 
-    nick.classList.remove("error");
+    nombre.classList.remove("error");
     age.classList.remove("error");
-    weapon.classList.remove("errorSub");
-    texto.classList.remove("error");
-
-    console.log(nick);
+    email.classList.remove("error");
 
 
+    if (nombre.value.trim() == "" || nombre.value.length > 20) {
+        errores.push("Nombre invalido, no puede estar vacio o tener mas de 20 caracteres");
+        nombre.classList.add("error");
+    }
+    let pattern = /^[A-Z]+$/i
 
-    if (nick.value.trim() == "") {
-        errores.push("Nick invalido, soquete");
-        nick.classList.add("error");
+    if (!pattern.test(nombre.value)) {
+        errores.push("Por favor, ingrese solo letras")
+        nombre.classList.add("error");
     }
 
 
     if (age.value.trim() == "") {
-        errores.push("Edad invalida, imbecil");
+        errores.push("Edad invalida, no puede estar vacia");
         age.classList.add("error");
     } else {
         if (isNaN(age.value)) {
-            errores.push("Edad invalida, imbecil");
+            errores.push("Edad invalida, no puede estar vacia");
             age.classList.add("error");
 
-        } else if (age.value < 0) {
-            errores.push("Edad invalida, imbecil");
+        } else if (age.value < 18 || age.value > 99) {
+            errores.push("Tienes que ser mayor de edad o tener menos de un siglo");
             age.classList.add("error");
         }
     }
 
-    if (!document.querySelector('input[name="opciones"]:checked')) {
-        errores.push("Debes seleccionar al menos una opcion, tarado");
-        weapon.classList.add("errorSub");
-    }
+    let er = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
-    if (texto.value.trim() == "" && habilidad.checked) {
-        errores.push("Tienes que dejar un mensaje, idiota");
-        texto.classList.add("error");
+    if (!er.test(email.value)) {
+        errores.push("Ingrese un correo electronico valido");
+        email.classList.add("error");
+        
     }
 
     let listaerrores = document.querySelector("ul");
